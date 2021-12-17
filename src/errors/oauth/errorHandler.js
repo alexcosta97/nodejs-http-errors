d251b36b647bd6b1520c17a8f5a5303f4b5c8d53
Alexandre Costa <alex.callum.c@gmail.com>
2021-12-17 11:30:10 +0000

    feat: improve logging of OAuth errors

--- src/errors/oauth/errorHandler.js
+++ src/errors/oauth/errorHandler.js
@@ -2,14 +2,14 @@ import OAuthError from './error';
 
 export default function (err, req, res) {
   if (err instanceof OAuthError && res) {
-    //Log as HttpError
-    console.error(`Error occured: ${err.message}`);
+    //Log as OAuthError
+    console.error(`Error occured of type ${err.error}: ${err.error_description}`);
     return res.status(err.status).json({
       error: err.error,
       error_description: err.error_description
     });
   } else {
     //Log as Normal Error
-    console.error(`Error occured: ${err.message}`);
+    console.error(`Error occured: ${err.message}\n${err.stack}`);
   }
 }
